520ef27acd7144e9dad21554e4df5ff3e9fd8d15
shinriyo <shinriyo@gmail.com>
2014-02-24 23:13:09 +0900

    coffee angular

--- csrf_sample/tok/static/coffee/main.coffee
+++ csrf_sample/tok/static/coffee/main.coffee
@@ -2,3 +2,7 @@ angular.module("SearchApp", [])
     .service("searchService", SearchService)
     .filter("andSearchFilter", ANDSearchFilter)
     .controller("searchController", SearchController)
+    .config(($interpolateProvider) ->
+        $interpolateProvider.startSymbol('{$')
+        $interpolateProvider.endSymbol('$}')
+    )
--- csrf_sample/tok/static/js/main.js
+++ csrf_sample/tok/static/js/main.js
@@ -1,4 +1,4 @@
 angular.module("SearchApp", []).service("searchService", SearchService).filter("andSearchFilter", ANDSearchFilter).controller("searchController", SearchController).config(function($interpolateProvider) {
-$interpolateProvider.startSymbol('{$');
-$interpolateProvider.endSymbol('$}');
+  $interpolateProvider.startSymbol('{$');
+  return $interpolateProvider.endSymbol('$}');
 });
